e613aa653bcefa755328abd6342f9ee375d94f60
103620582 <103620582@student.swin.edu.au>
2021-03-30 13:41:29 +1100

    Exercise2 completegit add .

--- Exercise2_Selection.cs
+++ Exercise2_Selection.cs
@@ -6,9 +6,38 @@ namespace Exercise2_Selection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I hope this works.");
             
+            Console.WriteLine("Please enter your username.");
+            
+            string username = Console.ReadLine();
+            
+            if (username == "jambrose")
+            {
+                Console.WriteLine("Please enter your password");
+                
+                string password = Console.ReadLine();
+
+                if (password == "potatoes")
+                {
+                    Console.WriteLine("Hang tight! We're logging you in.");
+                    Console.WriteLine("You're in! Welcome.");
+
+                }
+                else
+                {
+                    Console.WriteLine("Password incorrect.");
+                }
+
+            }
+            else if (username != "jambrose");
+            {
+                Console.WriteLine("Log in unsuccessful.");
+            }
+
+
+    
+
+
         }
     }
 }
