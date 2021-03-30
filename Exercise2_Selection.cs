32f20a02f4c9cedd4d51b403e8fcc83e4bbe627f
103620582 <103620582@student.swin.edu.au>
2021-03-30 12:43:49 +1100

    First Commit

---
+++ Exercise2_Selection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise2_Selection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I hope this works.");
+            
+        }
+    }
+}
